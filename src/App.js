9e45918ae85b29c6d115a07bb7a9c2fe92d84d04
Lucas Mendes Martins <martinslmendes@gmail.com>
2025-05-28 19:46:14 -0300

    Refactor: uuids for teams

--- src/App.js
+++ src/App.js
@@ -3,70 +3,197 @@ import Form from "./components/Form";
 import {useState} from "react";
 import Team from "./components/Team";
 import Footer from "./components/Footer";
+import {v4 as uuidv4} from 'uuid';
 
 const App = () => {
-    const [teams, setTeams] = useState([
-        {
-            name: 'Programação',
-            color: '#57C278',
-        },
-        {
-            name: 'Front-End',
-            color: '#82CFFA',
-        },
-        {
-            name: 'Data Science',
-            color: '#A6D157',
-        },
-        {
-            name: 'Devops',
-            color: '#E06B69',
-        },
-        {
-            name: 'UX e Design',
-            color: '#DB6EBF',
-        },
-        {
-            name: 'Mobile',
-            color: '#FFBA05',
-        },
-        {
-            name: 'Inovação e Gestão',
-            color: '#FF8A29',
-        },
-    ]);
-    const [employees, setEmployees] = useState([]);
+    const [teams, setTeams] = useState([{
+        id: uuidv4(), name: 'Programação', color: '#57C278',
+    }, {
+        id: uuidv4(), name: 'Front-End', color: '#82CFFA',
+    }, {
+        id: uuidv4(), name: 'Data Science', color: '#A6D157',
+    }, {
+        id: uuidv4(), name: 'Devops', color: '#E06B69',
+    }, {
+        id: uuidv4(), name: 'UX e Design', color: '#DB6EBF',
+    }, {
+        id: uuidv4(), name: 'Mobile', color: '#FFBA05',
+    }, {
+        id: uuidv4(), name: 'Inovação e Gestão', color: '#FF8A29',
+    },]);
+    const initial = [{
+        id: uuidv4(),
+        name: 'JULIANA AMOASEI',
+        role: 'Desenvolvedora de software e instrutora',
+        image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+        team: teams[0].name
+    }, {
+        id: uuidv4(),
+        name: 'DANIEL ARTINE',
+        role: 'Engenheiro de Software na Stone Age',
+        image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+        team: teams[0].name
+    }, {
+        id: uuidv4(),
+        name: 'GUILHERME LIMA',
+        role: 'Desenvolvedor Python e JavaScript na Alura',
+        image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+        team: teams[0].name
+    }, {
+        id: uuidv4(),
+        name: 'PAULO SILVEIRA',
+        role: 'Hipster e CEO da Alura',
+        image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+        team: teams[0].name
+    }, {
+        id: uuidv4(),
+        name: 'JULIANA AMOASEI',
+        role: 'Desenvolvedora de software e instrutora',
+        image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+        team: teams[1].name
+    }, {
+        id: uuidv4(),
+        name: 'DANIEL ARTINE',
+        role: 'Engenheiro de Software na Stone Age',
+        image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+        team: teams[1].name
+    }, {
+        id: uuidv4(),
+        name: 'GUILHERME LIMA',
+        role: 'Desenvolvedor Python e JavaScript na Alura',
+        image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+        team: teams[1].name
+    }, {
+        id: uuidv4(),
+        name: 'PAULO SILVEIRA',
+        role: 'Hipster e CEO da Alura',
+        image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+        team: teams[1].name
+    }, {
+        id: uuidv4(),
+        name: 'JULIANA AMOASEI',
+        role: 'Desenvolvedora de software e instrutora',
+        image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+        team: teams[2].name
+    }, {
+        id: uuidv4(),
+        name: 'DANIEL ARTINE',
+        role: 'Engenheiro de Software na Stone Age',
+        image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+        team: teams[2].name
+    }, {
+        id: uuidv4(),
+        name: 'GUILHERME LIMA',
+        role: 'Desenvolvedor Python e JavaScript na Alura',
+        image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+        team: teams[2].name
+    }, {
+        id: uuidv4(),
+        name: 'PAULO SILVEIRA',
+        role: 'Hipster e CEO da Alura',
+        image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+        team: teams[2].name
+    }, {
+        id: uuidv4(),
+        name: 'JULIANA AMOASEI',
+        role: 'Desenvolvedora de software e instrutora',
+        image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+        team: teams[3].name
+    }, {
+        id: uuidv4(),
+        name: 'DANIEL ARTINE',
+        role: 'Engenheiro de Software na Stone Age',
+        image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+        team: teams[3].name
+    }, {
+        id: uuidv4(),
+        name: 'GUILHERME LIMA',
+        role: 'Desenvolvedor Python e JavaScript na Alura',
+        image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+        team: teams[3].name
+    }, {
+        id: uuidv4(),
+        name: 'PAULO SILVEIRA',
+        role: 'Hipster e CEO da Alura',
+        image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+        team: teams[3].name
+    }, {
+        id: uuidv4(),
+        name: 'JULIANA AMOASEI',
+        role: 'Desenvolvedora de software e instrutora',
+        image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+        team: teams[4].name
+    }, {
+        id: uuidv4(),
+        name: 'DANIEL ARTINE',
+        role: 'Engenheiro de Software na Stone Age',
+        image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+        team: teams[4].name
+    }, {
+        id: uuidv4(),
+        name: 'GUILHERME LIMA',
+        role: 'Desenvolvedor Python e JavaScript na Alura',
+        image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+        team: teams[4].name
+    }, {
+        id: uuidv4(),
+        name: 'PAULO SILVEIRA',
+        role: 'Hipster e CEO da Alura',
+        image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+        team: teams[4].name
+    }, {
+        id: uuidv4(),
+        name: 'JULIANA AMOASEI',
+        role: 'Desenvolvedora de software e instrutora',
+        image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+        team: teams[5].name
+    }, {
+        id: uuidv4(),
+        name: 'DANIEL ARTINE',
+        role: 'Engenheiro de Software na Stone Age',
+        image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+        team: teams[5].name
+    }, {
+        id: uuidv4(),
+        name: 'GUILHERME LIMA',
+        role: 'Desenvolvedor Python e JavaScript na Alura',
+        image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+        team: teams[5].name
+    }, {
+        id: uuidv4(),
+        name: 'PAULO SILVEIRA',
+        role: 'Hipster e CEO da Alura',
+        image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+        team: teams[5].name
+    },]
+
+    const [employees, setEmployees] = useState(initial);
     const onEmployeeSubmit = (employee) => {
         setEmployees([...employees, employee]);
     }
     const deleteEmployee = (employee) => {
         //TODO
     }
-    const setTeamColor = (name, color) => {
+    const setTeamColor = (id, color) => {
         setTeams(teams.map(team => {
-            if (team.name === name) {
+            if (team.id === id) {
                 team.color = color;
             }
             return team;
         }));
     }
 
-    return (
-        <div className="App">
-            <Banner/>
-            <Form onSubmit={employee => onEmployeeSubmit(employee)} teams={teams.map(team => team.name)}/>
-            {teams.map((team) => (
-                <Team employees={employees.filter(employee => employee.team === team.name)}
-                      key={team.name}
-                      name={team.name}
-                      color={team.color}
-                      onDelete={deleteEmployee}
-                      onColorChange={setTeamColor}
-                />
-            ))}
-            <Footer/>
-        </div>
-    );
+    return (<div className="App">
+        <Banner/>
+        <Form onSubmit={employee => onEmployeeSubmit(employee)} teams={teams.map(team => team.name)}/>
+        {teams.map((team) => (<Team employees={employees.filter(employee => employee.team === team.name)}
+                                    key={team.id}
+                                    team={team}
+                                    onDelete={deleteEmployee}
+                                    onColorChange={setTeamColor}
+        />))}
+        <Footer/>
+    </div>);
 }
 
 export default App;
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,23 +1,16 @@
 import Employee from "../Employee";
 import hexToRgba from 'hex-to-rgba';
 
-export const Team = ({name, employees, color, onDelete, onColorChange}) => {
-    return (
-        (employees.length > 0) &&
-        <section className='team' style={{backgroundColor: hexToRgba(color, 0.6)}}>
-            <input type="color" value={color} onChange={e => onColorChange(name, e.target.value)}/>
-            <h3 style={{borderColor: color}}>{name}</h3>
+export const Team = ({team, employees, onDelete, onColorChange}) => {
+    return ((employees.length > 0) && <section className='team' style={{backgroundColor: hexToRgba(team.color, 0.6)}}>
+            <input type="color" value={team.color} onChange={e => onColorChange(team.id, e.target.value)}/>
+            <h3 style={{borderColor: team.color}}>{team.name}</h3>
             <div className="employees">
-                {employees.map(employee =>
-                    <Employee key={employee.name}
-                              color={color}
-                              employee={employee}
-                              name={employee.name}
-                              role={employee.role}
-                              image={employee.image}
-                              onDelete={onDelete}
-                    />)}
+                {employees.map(employee => <Employee key={employee.id}
+                                                     color={team.color}
+                                                     employee={employee}
+                                                     onDelete={onDelete}
+                />)}
             </div>
-        </section>
-    );
+        </section>);
 }

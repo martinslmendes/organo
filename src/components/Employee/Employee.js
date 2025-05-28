fe1891706ff9d696705d0d51b09558605cefacd9
Lucas Mendes Martins <martinslmendes@gmail.com>
2025-05-28 19:46:14 -0300

    Refactor: uuids for teams

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -1,16 +1,14 @@
-import { IoIosRemoveCircle } from "react-icons/io";
+import {IoIosRemoveCircle} from "react-icons/io";
 
-export const Employee = ({name, role, image, color, onDelete}) => {
-    return (
-        <div className="employee">
+export const Employee = ({employee, color, onDelete}) => {
+    return (<div className="employee">
             <IoIosRemoveCircle size={25} className="delete" onClick={onDelete}/>
             <div className="header" style={{backgroundColor: color}}>
-                <img src={image} alt={name}/>
+                <img src={employee.image} alt={employee.name}/>
             </div>
             <div className="footer">
-                <h4>{name}</h4>
-                <h5>{role}</h5>
+                <h4>{employee.name}</h4>
+                <h5>{employee.role}</h5>
             </div>
-        </div>
-    );
+        </div>);
 }

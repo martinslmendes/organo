3dbd226ec0936fbcbce665cbc47b661e6a6cbd71
Lucas Mendes Martins <martinslmendes@gmail.com>
2025-05-28 20:10:33 -0300

    Feat: delete employee

--- src/App.js
+++ src/App.js
@@ -3,164 +3,164 @@ import Form from "./components/Form";
 import {useState} from "react";
 import Team from "./components/Team";
 import Footer from "./components/Footer";
-import {v4 as uuidv4} from 'uuid';
+import {v4 as uuid} from 'uuid';
 
 const App = () => {
     const [teams, setTeams] = useState([{
-        id: uuidv4(), name: 'Programação', color: '#57C278',
+        id: uuid(), name: 'Programação', color: '#57C278',
     }, {
-        id: uuidv4(), name: 'Front-End', color: '#82CFFA',
+        id: uuid(), name: 'Front-End', color: '#82CFFA',
     }, {
-        id: uuidv4(), name: 'Data Science', color: '#A6D157',
+        id: uuid(), name: 'Data Science', color: '#A6D157',
     }, {
-        id: uuidv4(), name: 'Devops', color: '#E06B69',
+        id: uuid(), name: 'Devops', color: '#E06B69',
     }, {
-        id: uuidv4(), name: 'UX e Design', color: '#DB6EBF',
+        id: uuid(), name: 'UX e Design', color: '#DB6EBF',
     }, {
-        id: uuidv4(), name: 'Mobile', color: '#FFBA05',
+        id: uuid(), name: 'Mobile', color: '#FFBA05',
     }, {
-        id: uuidv4(), name: 'Inovação e Gestão', color: '#FF8A29',
+        id: uuid(), name: 'Inovação e Gestão', color: '#FF8A29',
     },]);
     const initial = [{
-        id: uuidv4(),
+        id: uuid(),
         name: 'JULIANA AMOASEI',
         role: 'Desenvolvedora de software e instrutora',
         image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
         team: teams[0].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'DANIEL ARTINE',
         role: 'Engenheiro de Software na Stone Age',
         image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
         team: teams[0].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'GUILHERME LIMA',
         role: 'Desenvolvedor Python e JavaScript na Alura',
         image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
         team: teams[0].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'PAULO SILVEIRA',
         role: 'Hipster e CEO da Alura',
         image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
         team: teams[0].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'JULIANA AMOASEI',
         role: 'Desenvolvedora de software e instrutora',
         image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
         team: teams[1].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'DANIEL ARTINE',
         role: 'Engenheiro de Software na Stone Age',
         image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
         team: teams[1].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'GUILHERME LIMA',
         role: 'Desenvolvedor Python e JavaScript na Alura',
         image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
         team: teams[1].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'PAULO SILVEIRA',
         role: 'Hipster e CEO da Alura',
         image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
         team: teams[1].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'JULIANA AMOASEI',
         role: 'Desenvolvedora de software e instrutora',
         image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
         team: teams[2].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'DANIEL ARTINE',
         role: 'Engenheiro de Software na Stone Age',
         image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
         team: teams[2].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'GUILHERME LIMA',
         role: 'Desenvolvedor Python e JavaScript na Alura',
         image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
         team: teams[2].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'PAULO SILVEIRA',
         role: 'Hipster e CEO da Alura',
         image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
         team: teams[2].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'JULIANA AMOASEI',
         role: 'Desenvolvedora de software e instrutora',
         image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
         team: teams[3].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'DANIEL ARTINE',
         role: 'Engenheiro de Software na Stone Age',
         image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
         team: teams[3].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'GUILHERME LIMA',
         role: 'Desenvolvedor Python e JavaScript na Alura',
         image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
         team: teams[3].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'PAULO SILVEIRA',
         role: 'Hipster e CEO da Alura',
         image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
         team: teams[3].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'JULIANA AMOASEI',
         role: 'Desenvolvedora de software e instrutora',
         image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
         team: teams[4].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'DANIEL ARTINE',
         role: 'Engenheiro de Software na Stone Age',
         image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
         team: teams[4].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'GUILHERME LIMA',
         role: 'Desenvolvedor Python e JavaScript na Alura',
         image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
         team: teams[4].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'PAULO SILVEIRA',
         role: 'Hipster e CEO da Alura',
         image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
         team: teams[4].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'JULIANA AMOASEI',
         role: 'Desenvolvedora de software e instrutora',
         image: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
         team: teams[5].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'DANIEL ARTINE',
         role: 'Engenheiro de Software na Stone Age',
         image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
         team: teams[5].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'GUILHERME LIMA',
         role: 'Desenvolvedor Python e JavaScript na Alura',
         image: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
         team: teams[5].name
     }, {
-        id: uuidv4(),
+        id: uuid(),
         name: 'PAULO SILVEIRA',
         role: 'Hipster e CEO da Alura',
         image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
@@ -171,9 +171,11 @@ const App = () => {
     const onEmployeeSubmit = (employee) => {
         setEmployees([...employees, employee]);
     }
-    const deleteEmployee = (employee) => {
-        //TODO
+
+    function deleteEmployee(employeeId) {
+        setEmployees(employees.filter(employee => employee.id !== employeeId));
     }
+
     const setTeamColor = (id, color) => {
         setTeams(teams.map(team => {
             if (team.id === id) {
--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -2,13 +2,13 @@ import {IoIosRemoveCircle} from "react-icons/io";
 
 export const Employee = ({employee, color, onDelete}) => {
     return (<div className="employee">
-            <IoIosRemoveCircle size={25} className="delete" onClick={onDelete}/>
-            <div className="header" style={{backgroundColor: color}}>
-                <img src={employee.image} alt={employee.name}/>
-            </div>
-            <div className="footer">
-                <h4>{employee.name}</h4>
-                <h5>{employee.role}</h5>
-            </div>
-        </div>);
+        <IoIosRemoveCircle size={25} className="delete" onClick={() => onDelete(employee.id)} />
+        <div className="header" style={{backgroundColor: color}}>
+            <img src={employee.image} alt={employee.name}/>
+        </div>
+        <div className="footer">
+            <h4>{employee.name}</h4>
+            <h5>{employee.role}</h5>
+        </div>
+    </div>);
 }
